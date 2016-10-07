d74fb41f45f61850bf4ee38aed41cded888211eb
Jai Srikar <gruprog@gmail.com>
2016-10-07 12:46:15 -0400

    setting up a basic ReactiveCocoa project with CocoaPods

---
+++ ReactiveCocoa/RACSetupWithCocoaPods/RACSetupWithCocoaPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RACSetupWithCocoaPods
+//
+//  Created by droid on 2016-10-07.
+//  Copyright © 2016 droid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
